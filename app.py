ae33e6b689ae4f7c79552c2f750bd857492f2cbc
Skarppinen <severi.karppinen@metropolia.fi>
2023-09-26 14:01:19 +0300

    updated code

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ import helpers.database_helpers as database
 from geopy import distance
 import random
 
-result = database.this_thing(input('anna iso_country: '))
+result = database.get_random_airports('foo')
 if result != 'ERROR':
     for y in result:
-        if y[1] != 'closed':
-            if y[1] == 'heliport':
-                print(y[0], 'IS A HELIPORT')
+        print(y, 'foobar')
 else:
     print('ERROR :(')
--- helpers/database_helpers.py
+++ helpers/database_helpers.py
@@ -1,11 +1,12 @@
 import helpers.connector as connector
-def this_thing(inp):
+
+
+def get_random_airports(inp):
     my_cursor = connector.mydb.cursor()
-    sql = f'SELECT name, type FROM airport WHERE iso_country="{inp.upper()}"'
+    sql = f'SELECT name, type FROM airport WHERE iso_country="US" ORDER BY RAND() LIMIT 1'
     my_cursor.execute(sql)
     data_result = my_cursor.fetchall()
     if data_result:
         return data_result
     else:
         return 'ERROR'
-
