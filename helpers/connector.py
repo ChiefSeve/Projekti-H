0c6bc5e9dc637b0d5814382e0acb3d7797c4fdbf
aaro <aaros.saila@gmail.com>
2023-09-29 13:03:24 +0300

    password korjattu

--- helpers/connector.py
+++ helpers/connector.py
@@ -6,6 +6,6 @@ load_dotenv()
 mydb = mysql.connector.connect(
     host=os.getenv('DB_HOST'),
     user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD_2'),
+    password=os.getenv('DB_PASSWORD'),
     database=os.getenv('DB_DATABASE')
 )
