9b0bff6c9ecdf25d39dca3d36ac1394ff1c5c95e
ChiefSeve <65978635+ChiefSeve@users.noreply.github.com>
2023-09-29 10:38:16 +0300

    Add files via upload

---
+++ flight_game.sql
@@ -0,0 +1,220 @@
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2023-09-26 13:03:14
+LastUsageStatisticCall<|||>1<|||>2023-09-18 13:07:59
+LastSessions<|||>1<|||>test
+LastActiveSession<|||>1<|||>test
+SessionManager_WindowWidth<|||>3<|||>1248
+SessionManager_WindowHeight<|||>3<|||>636
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+FileDialogEncoding_loaddataform<|||>3<|||>0
+CSVImportWindowWidth<|||>3<|||>725
+loadfilename<|||>1<|||>C:\Users\Karppinen\Downloads\countries.csv
+ColWidths_UserManagerForm.listUsers<|||>1<|||>97,80
+ColsVisible_UserManagerForm.listUsers<|||>1<|||>0,1
+ColPositions_UserManagerForm.listUsers<|||>1<|||>0,1
+ColSort_UserManagerForm.listUsers<|||>1<|||>0,0
+MainWinMaximized<|||>3<|||>1
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,233
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+SQL Attr Comment Foreground<|||>3<|||>8023636
+SQL Attr Comment Background<|||>3<|||>536870911
+SQL Attr Comment Style<|||>3<|||>2
+SQL Attr ConditionalComment Foreground<|||>3<|||>12108397
+SQL Attr ConditionalComment Background<|||>3<|||>536870911
+SQL Attr ConditionalComment Style<|||>3<|||>2
+SQL Attr ConsoleOutput Foreground<|||>3<|||>536870911
+SQL Attr ConsoleOutput Background<|||>3<|||>536870911
+SQL Attr ConsoleOutput Style<|||>3<|||>5
+SQL Attr DataType Foreground<|||>3<|||>15372999
+SQL Attr DataType Background<|||>3<|||>536870911
+SQL Attr DataType Style<|||>3<|||>1
+SQL Attr DefaultPackages Foreground<|||>3<|||>536870911
+SQL Attr DefaultPackages Background<|||>3<|||>536870911
+SQL Attr DefaultPackages Style<|||>3<|||>1
+SQL Attr DelimitedIdentifier Foreground<|||>3<|||>16757122
+SQL Attr DelimitedIdentifier Background<|||>3<|||>536870911
+SQL Attr DelimitedIdentifier Style<|||>3<|||>0
+SQL Attr Exception Foreground<|||>3<|||>536870911
+SQL Attr Exception Background<|||>3<|||>536870911
+SQL Attr Exception Style<|||>3<|||>2
+SQL Attr Function Foreground<|||>3<|||>14929603
+SQL Attr Function Background<|||>3<|||>536870911
+SQL Attr Function Style<|||>3<|||>1
+SQL Attr Identifier Foreground<|||>3<|||>16757122
+SQL Attr Identifier Background<|||>3<|||>536870911
+SQL Attr Identifier Style<|||>3<|||>0
+SQL Attr Key Foreground<|||>3<|||>14929603
+SQL Attr Key Background<|||>3<|||>536870911
+SQL Attr Key Style<|||>3<|||>1
+SQL Attr Number Foreground<|||>3<|||>7361535
+SQL Attr Number Background<|||>3<|||>536870911
+SQL Attr Number Style<|||>3<|||>0
+SQL Attr PLSQL-ReservedWord Foreground<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Background<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Style<|||>3<|||>1
+SQL Attr ProcName Foreground<|||>3<|||>536870911
+SQL Attr ProcName Background<|||>3<|||>536870911
+SQL Attr ProcName Style<|||>3<|||>0
+SQL Attr Space Foreground<|||>3<|||>536870911
+SQL Attr Space Background<|||>3<|||>536870911
+SQL Attr Space Style<|||>3<|||>0
+SQL Attr SQLPlus-Command Foreground<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Background<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Style<|||>3<|||>1
+SQL Attr String Foreground<|||>3<|||>8906947
+SQL Attr String Background<|||>3<|||>536870911
+SQL Attr String Style<|||>3<|||>0
+SQL Attr Symbol Foreground<|||>3<|||>12897152
+SQL Attr Symbol Background<|||>3<|||>536870911
+SQL Attr Symbol Style<|||>3<|||>0
+SQL Attr TableName Foreground<|||>3<|||>6911735
+SQL Attr TableName Background<|||>3<|||>536870911
+SQL Attr TableName Style<|||>3<|||>0
+SQL Attr Variable Foreground<|||>3<|||>7064575
+SQL Attr Variable Background<|||>3<|||>536870911
+SQL Attr Variable Style<|||>3<|||>0
+SQLColMatchingBraceForeground<|||>1<|||>$0028EFFF
+SQLColMatchingBraceBackground<|||>1<|||>$004D513B
+Theme<|||>1<|||>Windows10 Dark
+FieldColor_Numeric<|||>3<|||>16750469
+FieldColor_Real<|||>3<|||>13663613
+FieldColor_Text<|||>3<|||>7591283
+FieldColor_Binary<|||>3<|||>13203071
+FieldColor_Datetime<|||>3<|||>7566281
+FieldColor_Spatial<|||>3<|||>13553267
+FieldColor_Other<|||>3<|||>7586241
+FieldEditor_Set<|||>3<|||>1
+ColWidths_frmTableEditor.listColumns<|||>1<|||>58,100,152,90,60,65,50,117,130,139,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>902,100,80,120
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>789,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,289
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+dbtreewidth<|||>3<|||>301
+ExportSQL_Filenames<|||>1<|||>backup_flight_game<{{{><}}}>
+CreateDbCollation<|||>1<|||>latin1_swedish_ci
+CSVImportWindowHeight<|||>3<|||>531
+PromptSaveFileOnTabClose<|||>3<|||>0
+querymemoheight<|||>3<|||>372
+MemoEditorWidth<|||>3<|||>501
+MemoEditorHeight<|||>3<|||>201
+SessionManager_ListWidth<|||>3<|||>228
+SQLFile0<|||>1<|||>C:\Users\Karppinen\AppData\Roaming\HeidiSQL\Backups\query-tab-.sql
+Servers\test\SessionCreated<|||>1<|||>2023-09-18 13:13:01
+Servers\test\Host<|||>1<|||>127.0.0.1
+Servers\test\WindowsAuth<|||>3<|||>0
+Servers\test\CleartextPluginEnabled<|||>3<|||>0
+Servers\test\User<|||>1<|||>root
+Servers\test\Password<|||>1<|||>2
+Servers\test\LoginPrompt<|||>3<|||>0
+Servers\test\Port<|||>1<|||>3306
+Servers\test\NetType<|||>3<|||>0
+Servers\test\Compressed<|||>3<|||>0
+Servers\test\LocalTimeZone<|||>3<|||>0
+Servers\test\QueryTimeout<|||>3<|||>30
+Servers\test\KeepAlive<|||>3<|||>20
+Servers\test\FullTableStatus<|||>3<|||>1
+Servers\test\Databases<|||>1<|||>
+Servers\test\Library<|||>1<|||>libmariadb.dll
+Servers\test\Comment<|||>1<|||>
+Servers\test\StartupScriptFilename<|||>1<|||>
+Servers\test\TreeBackground<|||>3<|||>536870911
+Servers\test\SshExecutable<|||>1<|||>
+Servers\test\SSHtunnelHost<|||>1<|||>
+Servers\test\SSHtunnelHostPort<|||>3<|||>22
+Servers\test\SSHtunnelUser<|||>1<|||>
+Servers\test\SSHtunnelPassword<|||>1<|||>7
+Servers\test\SSHtunnelTimeout<|||>3<|||>4
+Servers\test\SSHtunnelPrivateKey<|||>1<|||>
+Servers\test\SSHtunnelPort<|||>3<|||>3307
+Servers\test\SSL_Active<|||>3<|||>0
+Servers\test\SSL_Key<|||>1<|||>
+Servers\test\SSL_Cert<|||>1<|||>
+Servers\test\SSL_CA<|||>1<|||>
+Servers\test\SSL_Cipher<|||>1<|||>
+Servers\test\IgnoreDatabasePattern<|||>1<|||>
+Servers\test\LogFileDdl<|||>3<|||>0
+Servers\test\LogFileDml<|||>3<|||>0
+Servers\test\LogFilePath<|||>1<|||>C:\Users\Karppinen\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\test\ServerVersionFull<|||>1<|||>10.10.2-MariaDB - mariadb.org binary distribution
+Servers\test\ConnectCount<|||>3<|||>7
+Servers\test\ServerVersion<|||>3<|||>101002
+Servers\test\LastConnect<|||>1<|||>2023-09-29 09:24:46
+Servers\test\lastUsedDB<|||>1<|||>flight_game
+Servers\test\RefusedCount<|||>3<|||>6
+Servers\test\flight_game|\MemoEditorHighlighter_id<|||>1<|||>
+Servers\test\QueryHistory\2<|||>1<|||>2023-09-18 13:53:00|flight_game|32|SELECT iso_country FROM airport;
+Servers\test\QueryHistory\5<|||>1<|||>2023-09-18 14:22:04|flight_game|156|SELECT * FROM airport WHERE TYPE != 'closed'
+Servers\test\QueryHistory\6<|||>1<|||>2023-09-18 14:22:18|flight_game|79|SELECT * FROM airport WHERE TYPE != 'closed' AND TYPE = 'heliport'
+Servers\test\QueryHistory\7<|||>1<|||>2023-09-18 14:22:34|flight_game|0|SELECT * FROM airport WHERE TYPE != 'closed' AND TYPE = 'heliport' AND iso_country ='FI'
+Servers\test\QueryHistory\8<|||>1<|||>2023-09-18 14:22:47|flight_game|0|SELECT * FROM airport WHERE TYPE = 'heliport' AND iso_country ='FI'
+Servers\test\QueryHistory\9<|||>1<|||>2023-09-18 14:42:49|flight_game|0|SELECT * FROM airport WHERE iso_country ='SE'
+Servers\test\QueryHistory\10<|||>1<|||>2023-09-19 13:54:21|fg_test|16|create table game<{{{><}}}>  (<{{{><}}}>      id           int auto_increment<{{{><}}}>          primary key,<{{{><}}}>      money        int(8)      null,<{{{><}}}>      location     varchar(10) null,<{{{><}}}>      screen_name  varchar(40) null,<{{{><}}}>      player_range int         null<{{{><}}}>  )<{{{><}}}>   charset = latin1;
+Servers\test\QueryHistory\11<|||>1<|||>2023-09-19 13:55:02|fg_test|16| create table goal<{{{><}}}> (<{{{><}}}>     id          int         not null<{{{><}}}>         primary key,<{{{><}}}>     name        varchar(40) null,<{{{><}}}>     money       int         null,<{{{><}}}>     probability int         null<{{{><}}}> )<{{{><}}}>     charset = latin1;<{{{><}}}> <{{{><}}}> insert into goal (id, name, money, probability)<{{{><}}}> values  (1, 'TOPAZ', 300, 4),<{{{><}}}>         (2, 'EMERALD', 600, 3),<{{{><}}}>         (3, 'RUBY', 1000, 2),<{{{><}}}>         (4, 'DIAMOND', 0, 1),<{{{><}}}>         (5, 'BANDIT', -1, 3);<{{{><}}}>
+Servers\test\QueryHistory\12<|||>1<|||>2023-09-19 13:55:30|fg_test|16| create table ports<{{{><}}}> (<{{{><}}}>     id      int auto_increment<{{{><}}}>         primary key,<{{{><}}}>     game    int                  null,<{{{><}}}>     airport varchar(11)          not null,<{{{><}}}>     goal    int                  null,<{{{><}}}>     opened  tinyint(1) default 0 null<{{{><}}}> );
+Servers\test\QueryHistory\13<|||>1<|||>2023-09-19 14:09:32|flight_game|0|SELECT * FROM airport;
+Servers\test\QueryHistory\1<|||>1<|||>2023-09-19 14:09:58|flight_game|16|SELECT * FROM country;
+Servers\test\QueryHistory\14<|||>1<|||>2023-09-19 14:15:03|flight_game|16|SELECT * FROM airport WHERE iso_country = 'DE';
+Servers\test\QueryHistory\15<|||>1<|||>2023-09-19 14:15:18|flight_game|0|SELECT * FROM airport WHERE iso_country = 'DE' AND TYPE = 'closed';
+Servers\test\QueryHistory\16<|||>1<|||>2023-09-19 14:16:38|flight_game|0|SELECT * FROM airport WHERE iso_country = 'DE' AND TYPE = 'closed' and municipality = 'Berlin';
+Servers\test\QueryHistory\17<|||>1<|||>2023-09-19 15:19:04|flight_game|32|SELECT * FROM country
+Servers\test\QueryHistory\19<|||>1<|||>2023-09-19 15:32:54|flight_game|94|SELECT * FROM airport WHERE iso_country = 'US'
+Servers\test\QueryHistory\4<|||>1<|||>2023-09-19 15:33:33|flight_game|94|SELECT * FROM airport WHERE iso_country = 'US' AND TYPE = 'small_airport'
+Servers\test\QueryHistory\20<|||>1<|||>2023-09-19 15:35:36|flight_game|94|SELECT * FROM airport WHERE iso_country = 'US' AND TYPE != 'closed'
+Servers\test\QueryHistory\21<|||>1<|||>2023-09-19 15:35:47|flight_game|78|SELECT * FROM airport WHERE iso_country = 'US' AND TYPE != 'closed' AND TYPE != 'heliport'
+Servers\test\QueryHistory\22<|||>1<|||>2023-09-26 13:46:59|flight_game|328|SELECT * FROM airport WHERE iso_country ='US'
+Servers\test\QueryHistory\23<|||>1<|||>2023-09-26 13:53:11|flight_game|0|SET foreign_key_checks = 0
+Servers\test\QueryHistory\24<|||>1<|||>2023-09-26 13:53:19|flight_game|687|DELETE FROM airport WHERE iso_country <> 'US'
+Servers\test\QueryHistory\18<|||>1<|||>2023-09-26 13:53:56|flight_game|125|DELETE FROM airport WHERE TYPE = 'closed'
+Servers\test\QueryHistory\25<|||>1<|||>2023-09-26 13:54:19|flight_game|156|DELETE FROM airport WHERE TYPE = 'heliport'
+Servers\test\QueryHistory\27<|||>1<|||>2023-09-26 13:54:30|flight_game|328|DELETE FROM airport WHERE TYPE = 'small_airport'
+Servers\test\QueryHistory\26<|||>1<|||>2023-09-26 13:55:00|flight_game|31|DELETE FROM airport WHERE TYPE = 'seaplane_base'
+Servers\test\QueryHistory\28<|||>1<|||>2023-09-26 13:55:21|flight_game|31|DELETE FROM airport WHERE TYPE = 'balloonport'
+Servers\test\QueryHistory\30<|||>1<|||>2023-09-26 15:20:48|flight_game|0|SELECT MAX(elevation_ft), NAME, municipality FROM airport
+Servers\test\QueryHistory\31<|||>1<|||>2023-09-26 15:42:53|flight_game|15|SELECT * FROM airport
+Servers\test\QueryHistory\3<|||>1<|||>2023-09-28 13:54:51|flight_game|0|INSERT INTO game (frustration, location, screen_name) VALUES (0,  'KONT', 'testi')
+Servers\test\QueryHistory\29<|||>1<|||>2023-09-28 14:13:29|flight_game|0|SELECT * FROM game
+Servers\test\QueryHistory\32<|||>1<|||>2023-09-28 14:13:54|flight_game|15|DELETE FROM game WHERE id = 8
+Servers\test\QueryHistory\33<|||>1<|||>2023-09-28 14:13:58|flight_game|0|DELETE FROM game WHERE id = 7
+Servers\test\QueryHistory\34<|||>1<|||>2023-09-28 14:14:00|flight_game|16|DELETE FROM game WHERE id = 6
+Servers\test\QueryHistory\35<|||>1<|||>2023-09-28 14:14:06|flight_game|0|SELECT * FROM game<{{{><}}}>
+Servers\test\QueryHistory\36<|||>1<|||>2023-09-28 15:08:45|flight_game|15|INSERT INTO weather (id, STATUS, tempature) VALUES (1, 'cloudy', 25)
+Servers\test\QueryHistory\37<|||>1<|||>2023-09-28 15:09:08|flight_game|0|SELECT * FROM weather
+Servers\test\QueryHistory\38<|||>1<|||>2023-09-29 09:36:00|flight_game|0|SET foreign_key_checks=0
