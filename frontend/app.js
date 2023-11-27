172ed2bcd28fbfa9508c0247d7535a672cd51781
Skarppinen <severi.karppinen@metropolia.fi>
2023-11-27 14:54:54 +0200

    Map now centers to USA

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([60.23, 24.74], 13);
+const map = L.map('map').setView([44.08, -99.71], 4);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
