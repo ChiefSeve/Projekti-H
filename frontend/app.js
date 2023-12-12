3e16543ab037d85963fb08c929b3e702a94e97ae
Skarppinen <severi.karppinen@metropolia.fi>
2023-12-12 11:29:42 +0200

    Removed fly button

--- frontend/app.js
+++ frontend/app.js
@@ -115,13 +115,13 @@ function updatedUserData(userData) {
 async function flyToAirport(icao) {
   const response = await fetch(`http://127.0.0.1:3000/fly?icao=${icao.toUpperCase()}&userId=${activeUser.id}`);
   const playerData =  await response.json();
-  console.log('playerData', playerData)
   if ('game_over' in playerData) {
     gameOverScreen(activeUser, userDialog);
     const nodes = [
       searchForm,
       distanceForm,
-      flyForm
+      flyForm,
+      map
     ]
     nodes.forEach(node => {
       node.setAttribute('style', 'display: none')
@@ -130,13 +130,11 @@ async function flyToAirport(icao) {
   }
   activeUser = updatedUserData(playerData);
   updateInfo(info, activeUser);
-  console.log('activeUser', activeUser);
 //   Jos backend onnistuu eli sijainti muuttuu, vaihetaan kartalla käyttäjän sijainti punaisella merkillä. Eli poistetaan Nykynen punainen merkki ja laitetaan tilalle sininen.
 //   Paikka mihin lennetään, sieltä poistetaan sininen merkki ja laitetaan tilalle punainen
 }
 
 async function drawOnLocation(icao){
-  console.log(icao, 'tarkastus')
   const response = await fetch('http://127.0.0.1:3000/airport/' + icao);
   const locData = await response.json();
   const markerplayer = L.marker([locData.latitude_deg, locData.longitude_deg], {
@@ -275,7 +273,6 @@ window.addEventListener('load', async function(evt) {
   const userData = await usersResp.json();
   // Create create/select user form
   if (userData){
-    console.log('userData success');
     await createUserSelectForm(userData);
     await createUser();
   } else {
